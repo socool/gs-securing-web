90b9611e07b2949b3f3198f784446a30302ff52d
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-09-13 22:44:25 +0700

    init project

---
+++ src/main/java/com/bcorpse/gssecuringweb/GsSecuringWebApplication.java
@@ -0,0 +1,12 @@
+package com.bcorpse.gssecuringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSecuringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSecuringWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bcorpse/gssecuringweb/GsSecuringWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bcorpse.gssecuringweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GsSecuringWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
